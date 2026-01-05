c0763910e82e3d183ceef2190d709f0fde68f9f1
reasons16 <bernadette.t.maddatu@accenture.com>
2026-01-05 03:03:28 +0000

    Update API URLs to use fuzzy-sniffle-r4qx779vgj96fw77p and allow codespace/localhost in ALLOWED_HOSTS

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-snys5cleugw$7u^0i+dq4wrmz$3+f2=l1p!=h(800_v#8hguvi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
